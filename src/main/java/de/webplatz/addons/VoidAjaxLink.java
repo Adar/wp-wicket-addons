99572ae647f16e9283c08458a60e107e6dcd5e75
Christian Senkowski <cs@2scale.net>
2015-08-03 14:27:53 +0200

    added VoidAjaxLink

---
+++ src/main/java/de/webplatz/addons/VoidAjaxLink.java
@@ -0,0 +1,26 @@
+package de.webplatz.addons;
+
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.model.IModel;import java.lang.Override;import java.lang.String;
+
+/**
+ * Created by chris on 03.08.15.
+ *
+ * VoidAjaxLink
+ */
+public class VoidAjaxLink<T> extends AjaxLink<T> {
+
+  public VoidAjaxLink(String id) {
+    super(id);
+  }
+
+  public VoidAjaxLink(String id, IModel<T> model) {
+    super(id, model);
+  }
+
+  @Override
+  public void onClick(AjaxRequestTarget ajaxRequestTarget) {
+    // nothing to do
+  }
+}
