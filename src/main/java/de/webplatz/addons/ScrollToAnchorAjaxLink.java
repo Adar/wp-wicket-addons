516b69c25c18405e827841dbbe47f9d41be8642a
Christian Senkowski <cs@2scale.net>
2016-01-07 14:12:22 +0100

    Code now passes checkstyle, PMD and FindBugs.

--- src/main/java/de/webplatz/addons/ScrollToAnchorAjaxLink.java
+++ src/main/java/de/webplatz/addons/ScrollToAnchorAjaxLink.java
@@ -1,30 +1,84 @@
+/**
+ * MIT License.
+ */
 package de.webplatz.addons;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.model.IModel;
 
 /**
- * Created by chris on 31.07.15.
+ * ScrollToAnchorAjaxLink.
  *
- * ScrollToAnchorAjaxLink
+ * @param <T> Type of link, p.e. String.
+ * @author Christian Senkowski (cs@e-cs.co)
+ * @version $Id$
+ * @since 31.07.15
  */
+@SuppressWarnings("unused")
 public class ScrollToAnchorAjaxLink<T> extends AjaxLink<T> {
+    /**
+     * Serial Version UID.
+     */
+    private static final long serialVersionUID = -3961769989136447178L;
+    /**
+     * Anchor where to scroll to.
+     */
+    private final transient String anchor;
 
-  private final String anchorName;
+    /**
+     * Construct with markup id, model and anchor name.
+     *
+     * @param markupid Wicket markup id.
+     * @param model Wicket model.
+     * @param anchor Anchor which to scroll to.
+     */
+    public ScrollToAnchorAjaxLink(final String markupid, final IModel<T> model,
+        final String anchor) {
+        super(markupid, model);
+        this.anchor = anchor;
+    }
 
-  public ScrollToAnchorAjaxLink(String id, String anchorName) {
-    super(id);
-    this.anchorName = anchorName;
-  }
+    /**
+     * Overriden onclick.
+     * Adds javaScript to the request target which scrolls to the anchor.
+     *
+     * @param target Request target where to add the scroll.
+     */
+    @Override
+    public final void onClick(final AjaxRequestTarget target) {
+        target.prependJavaScript(
+            String.format(
+                "%1s%2$s%3$s",
+                "$('html,body').animate({scrollTop: $(\"a[name='",
+                this.anchor,
+                "']\").offset().top - 50}, 'slow');"
+            )
+        );
+    }
 
-  public ScrollToAnchorAjaxLink(String id, IModel<T> model, String anchorName) {
-    super(id, model);
-    this.anchorName = anchorName;
-  }
+    /**
+     * Write Object.
+     *
+     * @param stream Stream to write to.
+     * @throws java.io.IOException if stream fails.
+     */
+    private void writeObject(final ObjectOutputStream stream)
+        throws java.io.IOException {
+        throw new java.io.NotSerializableException(getClass().getName());
+    }
 
-  @Override
-  public void onClick(AjaxRequestTarget ajaxRequestTarget) {
-    ajaxRequestTarget.prependJavaScript("$('html,body').animate({scrollTop: $(\"a[name='" + anchorName + "']\").offset().top - 50}, 'slow');");
-  }
+    /**
+     * Read Object.
+     *
+     * @param stream Stream to read from.
+     * @throws java.io.IOException if stream fails.
+     * @throws ClassNotFoundException if class read is invalid.
+     */
+    private void readObject(final ObjectInputStream stream)
+        throws java.io.IOException, ClassNotFoundException {
+        throw new java.io.NotSerializableException(getClass().getName());
+    }
 }
--- src/main/java/de/webplatz/addons/TopNavbar.java
+++ src/main/java/de/webplatz/addons/TopNavbar.java
@@ -1,32 +1,89 @@
+/**
+ * MIT License.
+ */
 package de.webplatz.addons;
 
 import de.agilecoders.wicket.core.markup.html.bootstrap.navbar.Navbar;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.List;
 import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.link.AbstractLink;
 
-import java.util.List;
-
+/**
+ * TopNavbar.
+ *
+ * @author Christian Senkowski (cs@e-cs.co)
+ * @version $Id$
+ * @since 03.08.15
+ */
+@SuppressWarnings("unused")
 public class TopNavbar extends Navbar {
-  private Component badgeLabel;
-  private transient List<AbstractLink> badgeDropDownLinkList;
+    /**
+     * Serial Version UID.
+     */
+    private static final long serialVersionUID = -3725636639523022220L;
+    /**
+     * Badge Label.
+     */
+    private final transient Component label;
+    /**
+     * Badge Link List.
+     */
+    private final transient List<AbstractLink> linklist;
 
-  public TopNavbar(String componentId) {
-    super(componentId);
-  }
+    /**
+     * Construct.
+     *
+     * @param markupid Wicket markup id.
+     * @param label Label Component.
+     * @param list Link list.
+     */
+    public TopNavbar(final String markupid, final Component label,
+        final List<AbstractLink> list) {
+        super(markupid);
+        this.label = label;
+        this.linklist = list;
+    }
 
-  public Component getBadgeLabel() {
-    return badgeLabel;
-  }
+    /**
+     * Badge Label.
+     *
+     * @return Wicket Component.
+     */
+    public final Component getLabel() {
+        return this.label;
+    }
 
-  public void setBadgeLabel(Component badgeLabel) {
-    this.badgeLabel = badgeLabel;
-  }
+    /**
+     * Get badge link list.
+     *
+     * @return Link List.
+     */
+    public final List<AbstractLink> getLinklist() {
+        return this.linklist;
+    }
 
-  public List<AbstractLink> getBadgeDropDownLinkList() {
-    return badgeDropDownLinkList;
-  }
+    /**
+     * Write Object.
+     *
+     * @param stream Stream to write to.
+     * @throws java.io.IOException if stream fails.
+     */
+    private void writeObject(final ObjectOutputStream stream)
+        throws java.io.IOException {
+        throw new java.io.NotSerializableException(getClass().getName());
+    }
 
-  public void setBadgeDropDownLinkList(List<AbstractLink> badgeDropDownLinkList) {
-    this.badgeDropDownLinkList = badgeDropDownLinkList;
-  }
+    /**
+     * Read Object.
+     *
+     * @param stream Stream to read from.
+     * @throws java.io.IOException if stream fails.
+     * @throws ClassNotFoundException if class read is invalid.
+     */
+    private void readObject(final ObjectInputStream stream)
+        throws java.io.IOException, ClassNotFoundException {
+        throw new java.io.NotSerializableException(getClass().getName());
+    }
 }
